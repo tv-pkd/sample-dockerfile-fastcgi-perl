5e6458c89d28871fd6597ae0e61947182a59ed56
Skyw Tiberius Alker <psyrkaosd@gmail.com>
2024-05-12 14:52:50 -0400

    Update index.pl

--- index.pl
+++ index.pl
@@ -2,5 +2,16 @@
 use strict;
 use warnings;
 
+use CGI;
+use DBI;
+use JSON;
+
+use strict;
+use warnings;
+
+use Switch;
+use WWW::Curl;
+use WWW::Curl::Easy;
+
 print "Content-type: text/plain\n\n";
 print "another change 2"
