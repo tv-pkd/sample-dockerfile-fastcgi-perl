df39215fd81e7dd6ae17b1553fc3f541146a2386
Skyw Tiberius Alker <psyrkaosd@gmail.com>
2024-05-12 14:54:55 -0400

    Update index.pl

--- index.pl
+++ index.pl
@@ -3,15 +3,7 @@ use strict;
 use warnings;
 
 use CGI;
-use DBI;
-use JSON;
 
-use strict;
-use warnings;
-
-use Switch;
-use WWW::Curl;
-use WWW::Curl::Easy;
 
 print "Content-type: text/plain\n\n";
 print "adding includes"
